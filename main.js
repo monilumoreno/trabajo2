dd10c62099f94d158f9285c8fc7cfa757212c028
monilumoreno <monilumoreno@gmail.com>
2021-08-24 15:44:01 -0500

    version1

---
+++ main.js
@@ -0,0 +1,27 @@
+// acceder al DOM
+
+function datos () {
+
+//creamos variables
+var $nombres = document.getElementById("txtNombres").value
+var $apellidos = document.getElementById("txtApellidos").value
+var $identificacion = document.getElementById("txtIdentificacion").value
+var $nIdentificacion = document.getElementById("txtNoIdentificacion").value   
+var $celular = document.getElementById("txtCelular").value
+var $correoE = document.getElementById("txtCorreoE").value
+var $password = document.getElementById("txtPassword").value
+
+alert ($nombre)
+
+const valores =[$nombres, $apellidos, $identificacion, $nIdentificacion, $celular, $correoE, $password]
+console.log(valores)
+
+}
+
+
+
+
+
+
+
+
