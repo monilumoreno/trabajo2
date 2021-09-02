798ac7c190ca86f32ba852108ddf064f9c3b5c77
monilumoreno <monilumoreno@gmail.com>
2021-09-01 20:16:46 -0500

    formulario

--- main.js
+++ main.js
@@ -1,5 +1,36 @@
 // acceder al DOM
 
+
+class Usuarios{
+
+    /*atributos de la clase*/
+    nombres
+    apellidos
+    identificacion
+    nIdentificacion
+    celular
+    correoE
+    password
+
+    /*creacion de metodos o constructores*/
+
+    constructor (pNombres, pApellidos, pIdentificacion, pnIdentificacion, pCelular, pCorreoE, pPassword){
+    this.nombres=pNombres
+    this.apellidos=pApellidos
+    this.identificacion=pIdentificacion
+    this.nIdentificacion=pnIdentificacion
+    this.celular=pCelular
+    this.correoE=pCorreoE
+    this.password=pPassword 
+    }
+
+    nombreCompleto(){
+
+        return this.nombres+' '+ this.apellidos 
+    }
+
+    }
+    
 function datos () {
 
 //creamos variables
@@ -11,14 +42,14 @@ var $celular = document.getElementById("txtCelular").value
 var $correoE = document.getElementById("txtCorreoE").value
 var $password = document.getElementById("txtPassword").value
 
-alert ($nombre)
 
-const valores =[$nombres, $apellidos, $identificacion, $nIdentificacion, $celular, $correoE, $password]
-console.log(valores)
+const datosUsuarios = new Usuarios($nombres, $apellidos, $identificacion, $nIdentificacion, $celular, $correoE, $password)
+const nombreCompleto= datosUsuarios.nombreCompleto()
+alert (`Señor:  ${nombreCompleto}`)
+console.log(datosUsuarios)
+
 
 }
-
-
 
 
 
